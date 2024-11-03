ecb330de8d56d1b0ac09d599015c5daab0a8b898
bmooreco <bmooreco@gmail.com>
2024-11-03 16:19:25 +0200

    daily challenge

---
+++ Python/Week 1/Day 1/excersises/daily_challenge_1.py
@@ -0,0 +1,16 @@
+# user_input = input("give me a string now ")
+
+# if len(user_input) < 10:
+#     print("give me more than that")
+# elif len(user_input) > 10:
+#     print("you did good now more")
+# else: 
+#     print("Perfect for today!")
+
+# user_input= ("hello world")
+# # print(user_input [0]) 
+
+# print(user_input [10])
+
+user_input= ("hello world")
+print(user_input[-1])
