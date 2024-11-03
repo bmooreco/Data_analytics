fef3a8f865839e0342260927a57c9bd681fd5c2e
bmooreco <bmooreco@gmail.com>
2024-11-03 15:38:30 +0200

    day 1 exersises baruch

--- Python/Week 1/Day 1/excersises/xp_1.py
+++ Python/Week 1/Day 1/excersises/xp_1.py
@@ -1,1 +1,62 @@
-print("Hello World")*4
+# print("Hello World\n"*4)
+
+# # question 2
+# print ((99**3)*8)
+
+# # question 3
+
+# #>>> 5 < 3 False
+# #>>> 3 == 3 True
+# #>>> 3 == "3"
+# #>>> "3" > 3 True
+# #>>> "Hello" == "hello" False
+
+# #Question 4
+# computer_brand= "dell"
+# print (f"I have",computer_brand, "and happy")
+
+# # Question 5
+
+# name= "bar"
+# age= "I am too old"
+# shoe_size = "46"
+# info = (f"hello i am {name} and {age} with {shoe_size} up my mouth")
+# print (info)
+
+# #question 6
+# a=4
+# b=5
+# if a>b: 
+#     print("hello world")
+# elif b>a:
+#     print ("learn math)")
+
+# question 7
+
+# number = int(input("show me the number"))
+
+# if number % 2 == 0:
+#     print("even shot.")
+# else:
+#     print("od out of luck.")
+
+# question 8
+
+# my_name = "gandalf"
+# user_name = input("how are you called boy?")
+
+# if user_name== my_name:
+#     print("oh you are of the blue")
+# else:
+#     print(f"Oh, {user_name}? fool of a took, {my_name} wizard")
+
+# question 9
+
+
+height = int(input("how tall you be?"))
+
+if height >= 145:
+    print("You stand tall to ride")
+else:
+    print ("shorty better luck next year")
+
