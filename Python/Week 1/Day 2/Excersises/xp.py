f0da5e23827c282636dd2145523d90311085b2dd
bmooreco <bmooreco@gmail.com>
2024-11-05 11:06:30 +0200

    daily challenges baruch moore

---
+++ Python/Week 1/Day 2/Excersises/xp.py
@@ -0,0 +1,25 @@
+my_numbers = {3,5,7,11,13,17}
+my_numbers.add(23)
+my_numbers.add(25)
+my_numbers.remove(17)
+print(my_numbers)
+number_list=list (my_numbers)
+sorted_number=sorted(number_list)
+print(sorted_number) 
+
+# question 2
+# in general you cannot add more to the tuple it is set. However my .add or.pop or .remove you can change it
+
+# # question 3
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# basket.remove("Banana")
+# # print(basket)
+
+# basket.remove("Blueberries")
+# # print(basket)
+# basket.append("kiwis")
+# basket.remove("Apples")
+# basket.insert(0,"Apples")
+# basket.remove("kiwis")
+# basket.insert(3,"kiwis")
+# print(basket)
